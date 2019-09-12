0e52b6bda8dfb183b4ceac5057c19580e1fd8ec7
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-09-12 10:21:51 +0600

    fix

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Jenssegers\Agent\Agent;
 use View;
 
 class BaseController extends Controller
 {
+    protected $agent;
     public $title = 'Кайрат Нуртас | ';
+
+    public function __construct()
+    {
+        $this->agent = new Agent();
+        View::share('agent', $this->agent);
+    }
 }
--- resources/views/pattern/header.blade.php
+++ resources/views/pattern/header.blade.php
@@ -8,18 +8,24 @@
     </div>
 </div>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    @if($agent->isMobile())
+    <a class="navbar-brand" href="#">Меню</a>
+    <button style="display: block;" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    @endif
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
+            <li class="nav-item @if($_SERVER['REQUEST_URI'] == '/') active @endif">
                 <a class="nav-link" href="{{ route('home') }}">Главная <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item @if($_SERVER['REQUEST_URI'] == '/page/qairat-nurtas-musics') active @endif">
                 <a class="nav-link" href="{{ route('page.show', ['alias' => 'qairat-nurtas-musics']) }}">Песни</a>
             </li>
             {{--<li class="nav-item">--}}
                 {{--<a class="nav-link" href="{{ route('page.show', ['alias' => 'test']) }}">Клипы</a>--}}
             {{--</li>--}}
-            <li class="nav-item">
+            <li class="nav-item @if($_SERVER['REQUEST_URI'] == '/page/biografiya-kayrata-nurtasa') active @endif">
                 <a class="nav-link" href="{{ route('page.show', ['alias' => 'biografiya-kayrata-nurtasa']) }}">Биография</a>
             </li>
 
