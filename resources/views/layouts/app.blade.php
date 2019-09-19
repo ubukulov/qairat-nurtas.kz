e1eed972855ec546d45722e35372a929df1e559d
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-09-19 11:25:52 +0600

    seotools install and configure fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,8 @@
     <!-- End Google Tag Manager -->
 
     <!-- Required meta tags -->
-    <link rel="icon" href="favicon.ico" type="image/x-icon" />
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="{{url('favicon.ico')}}" type="image/x-icon" />
+    <link rel="shortcut icon" href="{{url('favicon.ico')}}" type="image/x-icon" />
     <meta charset="utf-8">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
