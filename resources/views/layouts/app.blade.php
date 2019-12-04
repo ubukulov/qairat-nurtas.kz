606018cf47c29b59517901b146c7ed80642acc66
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-12-04 18:02:51 +0600

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,8 @@
     <!-- Required meta tags -->
     <link rel="icon" href="{{url('favicon.ico')}}" type="image/x-icon" />
     <link rel="shortcut icon" href="{{url('favicon.ico')}}" type="image/x-icon" />
-    <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta http-equiv="Content-Type" content="text/html;">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     {!! SEO::generate() !!}
 
